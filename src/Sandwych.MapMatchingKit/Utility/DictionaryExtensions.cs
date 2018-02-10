913dc638bdcce6de4def8ae4ab379245e08e53c2
Wei "oldrev" Li <liwei@sandwych.com>
2018-02-11 01:22:14 +0800

    fix DictionaryExtensions

--- src/Sandwych.MapMatchingKit/Utility/DictionaryExtensions.cs
+++ src/Sandwych.MapMatchingKit/Utility/DictionaryExtensions.cs
@@ -6,7 +6,7 @@ namespace Sandwych.MapMatchingKit.Utility
 {
     public static class DictionaryExtensions
     {
-        public static TValue GetOrNull<TKey, TValue>(this IDictionary<TKey, TValue> self, in TKey key) where TValue : class
+        public static TValue GetOrNull<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> self, in TKey key) where TValue : class
             => self.TryGetValue(key, out var value) ? value : null;
     }
 }

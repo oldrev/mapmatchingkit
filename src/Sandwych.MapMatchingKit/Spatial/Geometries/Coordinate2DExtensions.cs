cefd38e0a9fc5e92cd6a75ad3f993cb552e51093
Wei "oldrev" Li <liwei@sandwych.com>
2018-03-08 10:07:26 +0800

    revert last commit

--- src/Sandwych.MapMatchingKit/Spatial/Geometries/Coordinate2DExtensions.cs
+++ src/Sandwych.MapMatchingKit/Spatial/Geometries/Coordinate2DExtensions.cs
@@ -10,10 +10,10 @@ namespace Sandwych.MapMatchingKit.Spatial.Geometries
         //Add workaround for C# 7.2's bug
         //(this in Coordinate2D self)
 
-        public static GeoAPI.Geometries.Coordinate ToGeoAPICoordinate(in Coordinate2D self) =>
+        public static GeoAPI.Geometries.Coordinate ToGeoAPICoordinate(this Coordinate2D self) =>
             new GeoAPI.Geometries.Coordinate(self.X, self.Y);
 
-        public static GeoAPI.Geometries.IPoint ToGeoAPIPoint(in Coordinate2D self) =>
+        public static GeoAPI.Geometries.IPoint ToGeoAPIPoint(this Coordinate2D self) =>
             new Point(self.X, self.Y);
     }
 }
